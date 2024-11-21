c33e41f4b7a48453bd6d917c38cd6c5d267409b0
JavaThorman <jtskola0911@gmail.com>
2024-11-21 21:06:09 +0100

    Minor changes to include UI

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import sys
-from Program.program import start_program  # Importing the 'start_program' function from the 'program' module
+from UI.ui import JobApplicationTrackerUI
 
-# Prompting the user for input on whether they want to start the program
-choice = input("\n\nWould you like to start this program? (Y/N): ").lower()
-
-# Checking if the user entered 'Y' or 'yes' to start the program
-if choice in ["y", "yes"]:
-    start_program()  # Calling the function to start the program
-
-# If the user entered 'N' or 'no', exit the program with a message
-elif choice in ["n", "no"]:
-    sys.exit("Exiting program...")  # Gracefully exiting the program
+if __name__ == "__main__":
+    app = JobApplicationTrackerUI()
+    app.mainloop()
