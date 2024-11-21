9c81d55afee6900829ad82b26bea79adb82b773e
JavaThorman <jtskola0911@gmail.com>
2024-11-21 21:08:09 +0100

    Removed definition of DATABASE_NAME = "" The database now sets up job_tracker_db if there isn't one already. No need to define db name in config.py

--- Database/config.py
+++ Database/config.py
@@ -3,4 +3,4 @@
 DB_HOST = "localhost"  # or "127.0.0.1"
 DB_USER = "root"  # MySQL username
 DB_PASSWORD = "root"  # MySQL password
-DB_NAME = "job_tracker_db"  # The database name
+# No need to create or define database_name. It's created inside of database.py
